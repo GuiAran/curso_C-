b8078857ed41a4aefe4d9968e3ba7813f14fb8e6
Guilherme Arantes <judoka.pvh@gmail.com>
2019-04-02 10:38:25 -0400

    Projeto iniciado

---
+++ Course/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+
+            lista.Add("Maria");
+            lista.Add("Alex");
+            lista.Add("Joao");
+
+            foreach (string pessoa in lista)
+            {
+                Console.WriteLine(pessoa);
+            }
+
+
+        }
+    }
+}
